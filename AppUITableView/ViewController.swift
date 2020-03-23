61db3fe7c430301fada5468e9fc67a9b568df574
Mikhail <pasechnik89@mail.ru>
2020-03-23 21:21:24 +1000

    Initial Commit

---
+++ AppUITableView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AppUITableView
+//
+//  Created by Михаил on 23.03.2020.
+//  Copyright © 2020 Mikhail. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
